adb9b8d0c30add94d6017ee5755f82ee4f3ec238
Johnty <jmaake@retrorabbit.co.za>
2019-01-22 14:29:35 +0200

    My SQL task

--- data/createuser.sql
+++ data/createuser.sql
@@ -1,18 +1,18 @@
 USE [master]
 GO
-CREATE LOGIN [firstnamelastname] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.firstname.lastname], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+CREATE LOGIN [johntymaake] WITH PASSWORD=N'jMm@201312886' MUST_CHANGE, DEFAULT_DATABASE=[sql101.johnty.maake], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-use [sql101.firstname.lastname]
+use [sql101.johnty.maake]
 
 GO
 use [master]
 
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.johnty.maake]
 GO
-CREATE USER [firstnamelastname] FOR LOGIN [firstnamelastname]
+CREATE USER [johntymaake] FOR LOGIN [johntymaake]
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.johnty.maake]
 GO
-ALTER ROLE [db_owner] ADD MEMBER [firstnamelastname]
+ALTER ROLE [db_owner] ADD MEMBER [johntymaake]
 GO
